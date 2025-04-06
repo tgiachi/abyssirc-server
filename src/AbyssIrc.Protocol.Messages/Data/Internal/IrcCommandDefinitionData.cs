9476f99b68af1271329b39e94f5b5bb27d963062
squid <squid@stormwind.it>
2025-04-06 09:37:03 +0200

    feat(AbyssIrc.Core.csproj): update TargetFrameworks to include netstandard2.1 and add System.Text.Json package reference feat(StringUtils.cs): add input validation to ToSnakeCase method to prevent ArgumentNullException feat(AbyssIrc.Protocol.Messages.csproj): update TargetFrameworks to include netstandard2.1 and add LangVersion latest feat(WhoIsCommand.cs): add new class for WHOIS IRC command with methods to parse and write commands feat(IrcCommandDefinitionData.cs): change record to class for IrcCommandDefinitionData feat(IrcCommandParser.cs): change partial class to class for IrcCommandParser and update ParseAsync method to use Stopwatch.StartNew() and Stop() for measuring elapsed time

--- src/AbyssIrc.Protocol.Messages/Data/Internal/IrcCommandDefinitionData.cs
+++ src/AbyssIrc.Protocol.Messages/Data/Internal/IrcCommandDefinitionData.cs
@@ -2,6 +2,17 @@ using AbyssIrc.Protocol.Messages.Interfaces.Commands;
 
 namespace AbyssIrc.Protocol.Messages.Data.Internal;
 
-public record IrcCommandDefinitionData(IIrcCommand Command)
+public class IrcCommandDefinitionData
 {
+    public IIrcCommand Command { get; set; }
+
+    public IrcCommandDefinitionData(IIrcCommand command)
+    {
+        Command = command;
+    }
+
+    public IrcCommandDefinitionData()
+    {
+
+    }
 }
